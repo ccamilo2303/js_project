dcddaeac5359ea24ae185a9b415e1da00071ad95
Cristian Camilo <ccamilo2303@gmail.com>
2018-09-27 21:30:54 -0500

    se ajusta funcion guardado del formulario usuarios

--- app/scripts/controllers/users.js
+++ app/scripts/controllers/users.js
@@ -5,25 +5,26 @@
 
 function usersActionsController() {
     
-   
 
-}
-function guardar(){
-    var nombre = document.getElementById("nombre").value;
-    var usuario = document.getElementById("usuario").value;
-    var clave = document.getElementById("clave").value;
-
-    USUARIOS.push({'nombre' : nombre, 'usuario': usuario, 'clave' : clave});
-    var temp = "<table border='1'><tr><td>Nombre</td><td>Usuario</td><td>Clave</td></tr>";
-    for(let a of USUARIOS){
-        console.log(a);
-        let x = a;
-        temp+="<tr><td>"+x.nombre+"</td><td>"+x.usuario+"</td><td>"+x.clave+"</tr>"
+    document.getElementById("btnGuardar").onclick = function (){
+        var nombre = document.getElementById("nombre").value;
+        var usuario = document.getElementById("usuario").value;
+        var clave = document.getElementById("clave").value;
+    
+        USUARIOS.push({'nombre' : nombre, 'usuario': usuario, 'clave' : clave});
+        var temp = "<table border='1'><tr><td>Nombre</td><td>Usuario</td><td>Clave</td></tr>";
+        for(let a of USUARIOS){
+            console.log(a);
+            let x = a;
+            temp+="<tr><td>"+x.nombre+"</td><td>"+x.usuario+"</td><td>"+x.clave+"</tr>"
+        }
+        temp += "</table>"
+        document.getElementById("tablaTemp").innerHTML = temp;
+    
+        document.getElementById("nombre").value = '';
+        document.getElementById("usuario").value = '';
+        document.getElementById("clave").value = '';
     }
-    temp += "</table>"
-    document.getElementById("tablaTemp").innerHTML = temp;
 
-    document.getElementById("nombre").value = '';
-    document.getElementById("usuario").value = '';
-    document.getElementById("clave").value = '';
 }
+
